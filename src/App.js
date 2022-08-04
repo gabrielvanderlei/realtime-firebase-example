0b3dde24d83690f5bf2e2affdf3e55f5b453923b
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2022-08-04 12:30:29 +0000

    add multiple rooms support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase, onValue, ref, set } from "firebase/database";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -30,8 +30,11 @@ const analytics = getAnalytics(app);
 const database = getDatabase(app);
 
 
-function sendMessage(userId, message) {
-  set(ref(database, 'messages'), {
+function sendMessage(roomId, userId, message) {
+  const messagesRef = ref(database, 'messages/' + roomId);
+  const newMessageRef = push(messagesRef);
+
+  set(newMessageRef, {
     userId,
     message
   });
@@ -39,25 +42,44 @@ function sendMessage(userId, message) {
 
 function App() {
   const [messages, setMessages] = useState([]);  
+  const [roomId, setRoomId] = useState('HOME');
   const [userId, setUserId] = useState('');
   const [messageText, setMessageText] = useState('');  
-  const messagesRef = ref(database, '/messages');
 
-  onValue(messagesRef, (snapshot) => {
-    const data = snapshot.val();
-    setMessages(data);
-  });
+  useEffect(() => {
+    const messagesRef = ref(database, '/messages/' + roomId);
+
+    onValue(messagesRef, (snapshot) => {
+      const data = snapshot.val();
+      setMessages(data);
+    });
+  }, [roomId]);
 
   return (
     <div className="App">
-      Realtime
-      {JSON.stringify(messages)}
-
+      <h1>Realtime Chat</h1>
+      <div className="chatWindow">
+        {(!messages || messages.length == 0) ? (
+          <p>
+            Any messages available in the room <div className="roomId">{roomId}</div>, send one.
+          </p>
+        ) : (
+          <div>
+            { Object.values(messages).map(({message, userId}) => (
+              <div>
+                <div className="roomId">{roomId}</div> 
+                <div className="userId">{userId}</div>
+                <div className="message">{message}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
       <div>
-        Send a message
-        <input type="text" placeholder="User ID" onChange={(e) => { setUserId(e.target.value) }}/>
-        <input type="text" placeholder="Message" onChange={(e) => { setMessageText(e.target.value) }}/>
-        <input type="button" value="Submit" onClick={() => {sendMessage(userId, messageText)}} />      
+        <input type="text" className="textInput" placeholder="Room ID" value={roomId} onChange={(e) => { setRoomId(e.target.value) }}/>
+        <input type="text" className="textInput" placeholder="User ID" value={userId} onChange={(e) => { setUserId(e.target.value) }}/>
+        <input type="text" className="textInput" placeholder="Message" value={messageText} onChange={(e) => { setMessageText(e.target.value) }}/>
+        <input type="button" className="buttonInput" value="Submit" onClick={() => {sendMessage(roomId, userId, messageText)}} />      
       </div>
 
     </div>
